bd32868db5f1b225fa5d94aba1a24f13cdbf7265
vaibhav.hariani <vaibhav.hariani@gmail.com>
2025-03-18 17:50:43 -0400

    Table design tentatively complete.

--- final_project/flask_backend/app.py
+++ final_project/flask_backend/app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from sqlalchemy.sql import func
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -14,8 +15,86 @@ app.config['SQLALCHEMY_DATABASE_URI'] =\
 ##Postgre: postgresql://username:password@host:port/database_name
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+class Base(DeclarativeBase):
+    pass
 
-class Student(db.Model):
-    id = db.Column()
+db = SQLAlchemy(app, model_class=Base)
+##Structure is set to change
 
+##Fundamental Login & User models
+class Student_Login(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    uname: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    student_id: Mapped[int]
+
+class Student_Data(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+
+class Professor_Login(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str]
+    uname: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    prof_id: Mapped[int]
+
+class Professor_Data(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+
+class Dean_Login(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str]
+    uname: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    dean_id: Mapped[int]
+
+class Dean_Data(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+
+class Course_Archetype:
+     id: Mapped[int] = mapped_column(primary_key=True)
+     course_name: Mapped[str]
+     course_code: Mapped[str]
+     major_id: Mapped[int]
+
+class Semesters:
+     id: Mapped[int] = mapped_column(primary_key=True)
+     name: Mapped[str]     
+
+class Course:
+     id: Mapped[int] = mapped_column(primary_key=True)
+     Running: Mapped[bool]
+     prof_id: Mapped[int]
+     course_id: Mapped[int]
+     section_id: Mapped[int]
+     semester_id: Mapped[int]
+     course_breakdown: Mapped[str]
+
+class Assignment:
+     id: Mapped[int] = mapped_column(primary_key=True)
+     course_id: Mapped[int]
+     assignment_weight: Mapped[int]
+     Name: Mapped[str]
+
+class Student_Course_Layer:
+     id: Mapped[int] = mapped_column(primary_key=True)
+     student_id: Mapped[int]
+     course_id: Mapped[int]
+
+class Assignment_Grade:
+        id: Mapped[int] = mapped_column(primary_key=True)
+        submitted: Mapped[bool]
+        assignment_id: Mapped[int]
+        SCL_id: Mapped[int]       
+
+if __name__ == "__main__":
+        with app.app_context():
+        ##Create all the models
+                db.create_all()
+        
