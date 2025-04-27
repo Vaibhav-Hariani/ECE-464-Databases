d518d1a9471264f1afe342c523ba7bd546474ed2
Vaibhav Hariani <vaibhav.hariani@gmail.com>
2025-04-27 15:20:15 -0400

    seems  a few things were missing: updated.

---
+++ assign2/transformer.py
@@ -0,0 +1,18 @@
+from sentence_transformers import SentenceTransformer
+from redis.commands.search.query import Query
+from redis.commands.search.field import TextField, TagField, VectorField
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
+
+import numpy as np
+import redis
+
+##As a demo, using a miniLM to generate embeddings for each document, and then 
+schema = (
+    TextField("content"),
+    TagField("genre"),
+    VectorField("embedding", "HNSW", {
+        "TYPE": "FLOAT32",
+        "DIM": 384,
+        "DISTANCE_METRIC":"L2"
+    })
+)
